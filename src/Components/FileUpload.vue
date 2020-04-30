abefdf3815f91e5ed55e82966498145ea27836cb
riccardogiorato <61690514+riccardogioratoatoms@users.noreply.github.com>
2020-04-30 15:32:35 +0200

    feat(file_upload): pdf only input and 3 state of uploading

--- src/Components/FileUpload.vue
+++ src/Components/FileUpload.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="uploadPdfFields">
+        <div v-if="stateUpload === 'uploaded' || stateUpload === 'uploadedError'" class="uploadedText">
+            <p>{{message}}</p>
+            <div v-if="uploadedFileUrl !== ''">
+                <a :href="uploadedFileUrl" target="_blank">Uploaded File Preview</a>
+            </div>
+        </div>
+
+        <SpinnerLoader v-if="stateUpload === 'uploading'" />
+
         <form
+            v-if="stateUpload === 'notinserted'"
             method="post"
             enctype="multipart/form-data"
             class="box has-advanced-upload"
@@ -18,12 +28,14 @@
                         d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z"
                     />
                 </svg>
+                <img class="pdfIcon" alt="" src="https://cdn4.iconfinder.com/data/icons/file-extension-names-vol-8/512/24-512.png">
                 <input
                     id="pdfFileInput"
                     ref="filePdf"
                     type="file"
                     name="file"
                     class="box__file"
+                    accept="application/pdf"
                     @change="onSelect"
                 >
                 <label
@@ -36,10 +48,6 @@
             <div class="box__uploading">Uploading…</div>
             <input type="hidden" name="ajax" value="1">
         </form>
-        <h5>{{message}}</h5>
-        <div v-if="uploadedFileUrl !== ''">
-            <a :href="uploadedFileUrl" target="_blank">Uploaded File Preview</a>
-        </div>
     </div>
 </template>
 <style scoped>
@@ -62,15 +70,23 @@
     font-size: 1.25rem;
     background-color: #ffffff;
     position: relative;
-    padding: 100px 20px;
+    padding: 0px 20px;
+    height: 100%;
+}
+
+.box .pdfIcon{
+    height: 80px;
+    padding: 20px;
+    margin: 0 auto;
+    display: block;
 }
 
 .box.has-advanced-upload .box__icon {
+    padding-top: 100px;
     width: 100%;
-    height: 80px;
+    height: 70px;
     fill: #313131;
     display: block;
-    margin-bottom: 40px;
 }
 svg:not(:root) {
     overflow: hidden;
@@ -125,13 +141,19 @@ button, input[type=submit] {
 </style>
 <script>
 import axios from 'axios'
+import SpinnerLoader from '@/Components/SpinnerLoader.vue'
+
 export default {
     name: 'FileUpload',
+    components: {
+        SpinnerLoader
+    },
     data(){
         return {
             file: '',
             message: '',
-            uploadedFileUrl: ''
+            uploadedFileUrl: '',
+            stateUpload: 'notinserted'
         }
     },
     methods: {
@@ -139,14 +161,19 @@ export default {
             const allowedTypes = ['application/pdf']
             const file = this.$refs.filePdf.files[0]
             if (file){
-                this.file = file
+                const fileExtension = file.name.split('.').pop()
+                const fileSize = file.size > 3 * 1000 * 1000
                 if (!allowedTypes.includes(file.type)){
                     this.message = 'Filetype is wrong!!'
                 }
-                if (file.size > 3 * 1000 * 1000){
+                if (fileSize){
                     this.message = 'Too large, max size allowed is 500kb'
                 }
-                this.onSubmit()
+                if (allowedTypes.includes(file.type) && !fileSize && fileExtension === 'pdf'){
+                    this.file = file
+                    this.stateUpload = 'uploading'
+                    this.onSubmit()
+                }
             }
         },
         async onSubmit(){
@@ -156,9 +183,11 @@ export default {
             try {
                 response = await axios.post('http://localhost:80/upload', formData)
                 if (response.status === 200){
+                    this.stateUpload = 'uploaded'
                     this.uploadedFileUrl = response.data.data
                     this.message = response.data.message
                 } else {
+                    this.stateUpload = 'uploadedError'
                     throw response.data.error
                 }
             } catch (err){
