7b74b0652985ab8cc0443a841d56d6a71db8899a
riccardogiorato <61690514+riccardogioratoatoms@users.noreply.github.com>
2020-05-20 08:55:07 +0200

    all links should be opened in a safe target blank

--- src/Mixins/Markdown.vue
+++ src/Mixins/Markdown.vue
@@ -1,6 +1,15 @@
 <script>
 import * as marked from 'marked'
 
+const renderer = new marked.Renderer()
+renderer.link = function(href, title, text){
+    const link = marked.Renderer.prototype.link.call(this, href, title, text)
+    return link.replace('<a', '<a rel="noopener noreferrer" target=\'_blank\' ')
+}
+marked.setOptions({
+    renderer
+})
+
 export default {
     methods: {
         marked
